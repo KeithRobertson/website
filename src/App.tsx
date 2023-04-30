a15469a4c861aaa023041284604a06ff999050b9
keith <keith@krobertson.com>
2023-04-30 16:36:38 +0100

    Update title and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React, {StrictMode} from 'react';
+import React, {StrictMode, useEffect} from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./component/Home/Home";
 import Library from "./component/Library/Library";
 
 function App() {
+    useEffect(() => {
+        document.title = "Keith Robertson";
+    }, []);
+
     return (
         <StrictMode>
             <BrowserRouter>
